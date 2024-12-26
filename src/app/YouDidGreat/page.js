62190f0c50f86178c07a22efa93594824641b867
mohammad asad <asadkhan41376@gmail.com>
2024-12-26 19:51:33 +0530

    first commit

---
+++ src/app/YouDidGreat/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen flex justify-center items-center '>
+   <h1 className='text-4xl text-green-800'>   You did great!</h1>
+    </div>
+  )
+}
+
+export default page
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,157 @@
-import Image from "next/image";
+
+"use client"
+import Image from 'next/image'
+import React, { useRef, useState } from 'react'
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Button } from "@/components/ui/button"
+import { Navigation } from "swiper/modules";
+import "swiper/css/navigation";
+import { MdDoubleArrow } from "react-icons/md";
+import { useRouter } from 'next/navigation';
+
+
+
+
+
+
+const SwierData = [
+  {
+    Image1: "/SlideImg/fish.jpg",
+    Image2: "/SlideImg/flipper.jpg",
+
+    firstHeading: "Fish",
+    SecoundHeading: "Flipper"
+
+  },
+
+  {
+    Image1: "/SlideImg/animal.jpg",
+    Image2: "/SlideImg/Velcro.jpg",
+    firstHeading: "Animal Fur and Burs",
+    SecoundHeading: "Velcro"
+
+  },
+
+  {
+    Image1: "/SlideImg/bird.jpg",
+    Image2: "/SlideImg/buuletTrain.jpg",
+
+    firstHeading: "KingFishier ",
+    SecoundHeading: "Buulet Train"
+
+  },
+  
+  {
+    Image1: "/SlideImg/flower (1).jpg",
+    Image2: "/SlideImg/color.jpg",
+
+    firstHeading: "Flower",
+    SecoundHeading: "paint colors"
+
+  },
+  {
+    Image1: "/SlideImg/woodpecker.jpg",
+    Image2: "/SlideImg/football.jpg",
+
+    firstHeading: "Woodpecker",
+    SecoundHeading: "Football"
+
+  },
+]
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const MySwipe = useRef()
+  const [isLastSlide, setIsLastSlide] = useState(false);
+  const router = useRouter()
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  // for handleClick Swiper Button Manual
+  const handleClick = () => {
+
+    if (!isLastSlide) {
+
+      MySwipe.current?.slideNext();
+    }
+    else {
+      router.push("/YouDidGreat")
+    }
+  }
+
+  return (
+    <>
+      <Swiper
+        onSlideChange={(swiper) => {
+          setIsLastSlide(swiper.activeIndex == SwierData.length - 1)
+        }}
+        onSwiper={(swipe) => {
+          MySwipe.current = swipe;
+        }}
+        modules={[Navigation]}
+        allowTouchMove={false}
+
+      >
+
+        {
+          SwierData.map((item, index) => (
+            <SwiperSlide className='relative' key={index}>
+
+
+              {/* YE APNA CONTAINT SLIDE KA  */}
+
+              <div className=' h-screen w-full flex  justify-around items-center  '>
+
+{/* first image ============================ */}
+
+                <div className='bg-[#79DAFF] h-full w-full flex items-center justify-center'>
+                  <div className='imageFirst w-[500px] h-[400px] relative rounded-2xl overflow-hidden shadow-md shadow-black'>
+                    <Image src={item.Image1} layout='fill' objectFit='cover' alt='image' />
+                    <h1 className='absolute bottom-0 text-center w-full py-2 bg-white text-3xl  text-black '>{item.firstHeading}</h1>
+                  </div>
+                </div>
+
+
+{/* secound image ============================ */}
+
+                <div className=" lg:bg-[#99A1B3] h-full w-full flex items-center justify-center ">
+
+
+                  <div className='imageSecound w-[500px] h-[400px] relative rounded-2xl overflow-hidden shadow-md shadow-black'>
+                    <Image src={item.Image2} layout='fill' objectFit='cover' alt='image' />
+                    <h1 className='absolute bottom-0 text-center w-full py-2 bg-white text-3xl  text-black '>{item.SecoundHeading}</h1>
+                  </div>
+                </div>
+
+              </div>
+
+              {/* YE APNA BTN   */}
+              <div className="absolute top-0 flex justify-between items-center w-full flex-col h-full py-5  ">
+                <div className='text-center  ' >
+                  <h1 className='text-3xl font-bold'>Design</h1>
+                  <p className='text-sm max-w-full px-8 '>Fish| Flipper
+Burs sticking on animal fur | Velcro
+Bird with pointed beak| Bullet train
+Flower | Paint colours
+Woodpecker | Football helmet
+                  </p>
+
+                </div>
+
+                <div className='relative myBtn '>
+
+                  <Button variant="outline" onClick={handleClick} className="bg-black text-xl text-white  hover:bg-transparent   h-[50px] py-4 px-8 w-[150px] btn " >Next <MdDoubleArrow /></Button>
+                </div>
+              </div>
+
+            </SwiperSlide>
+          ))
+        }
+
+
+
+      </Swiper>
+
+    </>
+
+
   );
 }
